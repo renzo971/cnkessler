e60ba180c2d750616899878d7ba82656724fc8c3
Renzo Santos <orrego.st.santos@gmail.com>
2025-09-30 23:49:40 -0500

    asdd cosas

---
+++ src/components/icons/icons.jsx
@@ -0,0 +1,191 @@
+export const Neurología = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M12 3c2 0 3 1 3 3s1 3 1 5-1 3-1 5-1 3-3 3-3-1-3-3-1-3-1-5 1-3 1-5 1-3 3-3z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8 12h8M10 8v8M14 8v8"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+export const Psiquiatría = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M12 2a5 5 0 00-5 5v1a5 5 0 005 5 5 5 0 005-5V7a5 5 0 00-5-5z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M8 17a4 4 0 018 0v3H8v-3z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+export const Oftalmología = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M2 12s4-7 10-7 10 7 10 7-4 7-10 7S2 12 2 12z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <circle cx="12" cy="12" r="3" />
+    </svg>
+  );
+};
+export const Neuropediatría = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <circle
+        cx="12"
+        cy="8"
+        r="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M5 20v-1a6 6 0 0114 0v1"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+export const Traumatología = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M5 12c0 1.5 1 3 3 3s3-1.5 3-3-1-3-3-3-3 1.5-3 3zM19 12c0 1.5-1 3-3 3s-3-1.5-3-3 1-3 3-3 3 1.5 3 3z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path d="M8 12h8" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
+};
+export const Otorrinolaringología = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M20 12c0 4-4 8-8 8s-8-4-8-8 4-8 8-8c1.5 0 3 .5 4 1.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path d="M12 8v8" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
+};
+
+export const Neurocirugía = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M3 21l18-9L3 3v7l12 2-12 2v7z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path d="M14 10v4" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
+};
+export const Reumatología = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path d="M7 7l10 10" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M7 17l10-10" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
+};
+export const Endocrinología = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="w-6 h-6"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="1.5"
+      aria-hidden="true"
+    >
+      <path
+        d="M8 2h8l2 6-2 6H8L6 8z"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path d="M12 14v6" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+  );
+};
