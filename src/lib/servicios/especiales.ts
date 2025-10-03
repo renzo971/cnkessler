674ee39910571b75ced344a86e105fcff022c689
Renzo Santos <orrego.st.santos@gmail.com>
2025-10-02 22:39:28 -0500

    intetno

--- src/lib/servicios/especiales.ts
+++ src/lib/servicios/especiales.ts
@@ -1,19 +1,16 @@
-//import servicio1 from "@/assets/servicios/especiales/servicio1.webp";
-//import servicio3 from "@/assets/servicios/especiales/servicio3.webp";
-import servicio4 from "@/assets/servicios/especiales/servicio4.webp";
+import servicio1 from "@/assets/servicios/generales/servicio1.webp";
+import servicio4 from "@/assets/servicios/generales/servicio4.webp";
+import servicio3 from "@/assets/servicios/generales/servicio3.jpg";
+import servicio2 from "@/assets/servicios/generales/servicio2.webp";
 export const serviciosespeciales = [
   {
-    titulo: " Vídeoelectroencefalograma nocturno y prolongado",
-    imagen: servicio4,
+    titulo: "Potenciales Evocados Auditivos",
+    imagen: servicio1,
   },
+  { titulo: "Neuroconducción", imagen: servicio2 },
   {
-    titulo:
-      "Pruebas especiales para la valoración del sistema nervioso autónomo",
-    imagen: servicio4,
+    titulo: "Electroencefalograma",
+    imagen: servicio3,
   },
-  {
-    titulo: "Test de Jolly ",
-    imagen: servicio4,
-  },
-  { titulo: "Electromiografía de segmentos especiales", imagen: servicio4 },
+  { titulo: "Electromiografía", imagen: servicio4 },
 ];
