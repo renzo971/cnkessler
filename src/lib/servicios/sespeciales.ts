f96bf7a38795cfad3c13d406cbc25d9e872cad0c
Renzo Santos <orrego.st.santos@gmail.com>
2025-10-02 22:14:14 -0500

    add webp image

--- src/lib/servicios/sespeciales.ts
+++ src/lib/servicios/sespeciales.ts
@@ -1,6 +1,6 @@
-import servicio1 from "@/assets/servicios/especiales/servicio1.jpg";
-import servicio3 from "@/assets/servicios/especiales/servicio3.jpg";
-import servicio4 from "@/assets/servicios/especiales/servicio4.jpg";
+import servicio1 from "@/assets/servicios/especiales/servicio1.webp";
+import servicio3 from "@/assets/servicios/especiales/servicio3.webp";
+import servicio4 from "@/assets/servicios/especiales/servicio4.webp";
 export const serviciosespeciales = [
   {
     titulo: " Vídeoelectroencefalograma nocturno y prolongado",
