ed804d41e33c0fc1305e41563f97615499093c9c
Renzo Santos <orrego.st.santos@gmail.com>
2025-10-02 22:58:31 -0500

    fix

--- src/lib/servicios/index.ts
+++ src/lib/servicios/index.ts
@@ -1,4 +1,4 @@
 import { serviciosgenerales } from "@/lib/servicios/generales";
-import { serviciosespeciales } from "@/lib/servicios/especiales";
+import { serviciosespeciales } from "@/lib/servicios/specials";
 import { medicinas } from "@/lib/servicios/medicina";
 export { serviciosgenerales, serviciosespeciales, medicinas };
--- src/lib/servicios/specials.ts
+++ src/lib/servicios/specials.ts
@@ -1,6 +1,6 @@
-import servicio1 from "@/assets/servicios/especiales/servicio1.webp";
-import servicio3 from "@/assets/servicios/especiales/servicio3.webp";
-import servicio4 from "@/assets/servicios/especiales/servicio4.webp";
+import servicio1 from "@/assets/servicios/specials/servicio1.webp";
+import servicio3 from "@/assets/servicios/specials/servicio3.webp";
+import servicio4 from "@/assets/servicios/specials/servicio4.webp";
 export const serviciosespeciales = [
   {
     titulo: " Vídeoelectroencefalograma nocturno y prolongado",
